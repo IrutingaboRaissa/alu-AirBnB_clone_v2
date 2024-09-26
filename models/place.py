374b5f7243cc604919f6f03d58bf251db51a271d
IrutingaboRaissa <r.irutingab2alustudent.com>
2024-09-26 13:10:34 +0200

    6

--- models/place.py
+++ models/place.py
@@ -1,108 +1,35 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Place Module for HBNB project """
-
-=======
-"""This is the place class"""
+"""This is the state class"""
 from sqlalchemy.ext.declarative import declarative_base
->>>>>>> parent of 06df725 (models)
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-<<<<<<< HEAD
-
-
-class Place(BaseModel, Base):
-    """ Place class """
-    __tablename__ = 'places'
-
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-                          Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
-                          )
-
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-=======
-from os import getenv
+from sqlalchemy import Column, Integer, String
 import models
+from models.city import City
+import shlex
 
 
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
-
-
-class Place(BaseModel, Base):
-    """This is the class for Place
+class State(BaseModel, Base):
+    """This is the class for State
     Attributes:
-        city_id: city id
-        user_id: user id
-        name: name input
-        description: string of description
-        number_rooms: number of room in int
-        number_bathrooms: number of bathrooms in int
-        max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
-        longitude: longitude in float
-        amenity_ids: list of Amenity ids
+        name: input name
     """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
->>>>>>> parent of 06df725 (models)
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-<<<<<<< HEAD
-    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False)
-=======
-    amenity_ids = []
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                               backref="place")
-
-        amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False,
-                                 back_populates="place_amenities")
-    else:
-        @property
-        def reviews(self):
-            """ Returns list of reviews.id """
-            var = models.storage.all()
-            lista = []
-            result = []
-            for key in var:
-                review = key.replace('.', ' ')
-                review = shlex.split(review)
-                if (review[0] == 'Review'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.place_id == self.id):
-                    result.append(elem)
-            return (result)
-
-        @property
-        def amenities(self):
-            """ Returns list of amenity ids """
-            return self.amenity_ids
-
-        @amenities.setter
-        def amenities(self, obj=None):
-            """ Appends amenity ids to the attribute """
-            if type(obj) is Amenity and obj.id not in self.amenity_ids:
-                self.amenity_ids.append(obj.id)
->>>>>>> parent of 06df725 (models)
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
