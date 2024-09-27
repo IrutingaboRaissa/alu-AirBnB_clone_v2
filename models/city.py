88e5b12f727a3f6705235055a6222885dcd30ded
Michael Musa Musembi <155955130+MichaelMusembi@users.noreply.github.com>
2024-09-27 10:52:56 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,38 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import Place
-
-
-class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
-=======
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
-
-    __tablename__ = 'cities'
-
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities")
->>>>>>> d5b6b5a181413a2cf8c40f815d0b8f9ab71ab051
+    state_id = ""
+    name = ""
