62b7105e7f90d2ad3d7fa9e095b2fbf62f462de0
IrutingaboRaissa <156107881+IrutingaboRaissa@users.noreply.github.com>
2024-10-10 15:44:10 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -41,15 +41,13 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
-        new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    new_dict[key] = obj
-        return (new_dict)
+  """query on the current database session and count objects of each class"""
+  new_dict = {}
+  for clss in classes:
+    if cls is None or cls is classes[clss] or cls is clss:
+      objs = self.__session.query(classes[clss]).all()
+      new_dict[clss] = len(objs)
+    return new_dict
 
     def new(self, obj):
         """add the object to the current database session"""
