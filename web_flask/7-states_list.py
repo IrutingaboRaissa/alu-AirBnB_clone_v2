a8ba8de0aeae0d5f0738ff312c362c1f73681cfd
IrutingaboRaissa <r.irutingab2alustudent.com>
2024-10-12 23:22:11 +0200

    7.8.9

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Module that starts a Flask web application."""
-
-import os
-import sys
 from flask import Flask, render_template
 from models import storage
 from models.state import State
